4821a04230c19c2a22e392c01d92430ad2867435
jem <fearlessjem17@gmail.com>
2020-02-17 18:16:05 +0800

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>hat page</h1>
-    </div>
-  );
-};
 
 const TopicsList = () => {
   return (
@@ -35,7 +28,7 @@ function App() {
     <div>
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage} />
       <Route path='topics/:id' component={TopicDetails} />
     </Switch>
 
---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, price, imageUrl, match}) => {
+    return (
+        <div className='collection-item'>
+            <div 
+                className='image' 
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+
+            <div className='collection-footer'>
+                <span className='name'> {name} </span>
+                <span className='price'> {price} </span>
+            </div>
+        </div>
+    ); 
+};
+
+export default withRouter(CollectionItem);
